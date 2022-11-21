bf779208d459851db766597df3531113e6c98672
Muhammad Hussain <118740883+MuhammadHussain003@users.noreply.github.com>
2022-11-21 20:58:32 +0500

    Create if and else.cpp

---
+++ if and else.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	int b;
+	a=20;
+	b=30;
+	if(a==b)
+	{
+		cout<<"a and b are equal";
+	}
+	else
+	{
+		cout<<"a and b are not equal";
+	}
+	
+}
