5164eeedd876e39e9af75be54552781291ac8049
Muhammad Hussain <118740883+MuhammadHussain003@users.noreply.github.com>
2022-11-21 21:20:55 +0500

    Create Swaping.cpp

---
+++ Swaping.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=10;
+	int b=20;
+	cout<<"Before Swaping"<<endl;
+	cout<<"a ="<<a<<endl;
+	cout<<"b ="<<b<<endl;
+	cout<<"After Swaping"<<endl;
+	cout<<"a = "<<b<<endl;
+	cout<<"b = "<<a; 
+}
