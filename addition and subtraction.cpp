17eb3886ae612ba34171b75d61da73692cd89445
Muhammad Hussain <118740883+MuhammadHussain003@users.noreply.github.com>
2022-11-21 20:52:24 +0500

    Create addition and subtraction.cpp

---
+++ addition and subtraction.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	int b;
+	a=20;
+	b=30;
+	int sum;
+	int subtract;
+	sum=a+b;
+	subtract=a-b;
+	cout<< sum<<endl;
+	cout<< subtract;
+	}
