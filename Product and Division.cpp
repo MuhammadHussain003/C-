69e6e63e0e286ea0db08a7b331a0d73db7416677
Muhammad Hussain <118740883+MuhammadHussain003@users.noreply.github.com>
2022-11-21 21:35:23 +0500

    Create Product and Division.cpp

---
+++ Product and Division.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=10;
+	int b=20;
+    int product;
+	int Division;
+	product=2*2;
+	Division=4/2;
+	cout<<product<<endl;
+	cout<<Division; 
+}
