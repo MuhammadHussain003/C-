2696423824d29ca56078962c7aad9c67ddd8fd02
Muhammad Hussain <118740883+MuhammadHussain003@users.noreply.github.com>
2022-11-22 23:20:33 +0500

    Create whileloop.cpp

---
+++ whileloop.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int b=1;
+	while(b<=1)
+	{
+		int valueOne;
+		int valueTwo;
+		cout<<"Enter valueOne";
+    	cin>>valueOne;
+    	cout<<"Enter valueTwo";
+    	cin>>valueTwo;
+	    cout<<"the result is"<<valueOne+valueTwo<<endl;
+    	cout<<"if you want to continue program press 1 otherwise press any key to stop";
+    	cin>>b;
+
+	}
+	
+    	
+        cout<<"Thank you";
+    	
+	
+	
+
+}
