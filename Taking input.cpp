edaea5cc0516c775a23a8231cf3b5c7aace77afb
Muhammad Hussain <118740883+MuhammadHussain003@users.noreply.github.com>
2022-11-29 19:33:01 +0500

    Create Taking input.cpp

---
+++ Taking input.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a , b;
+ cout<<"Enter First value =";
+ cin>>a;
+ cout<<"Enter Second value =";
+ cin>>b;
+ cout<<a<<"+"<<b<<"="<<a+b;
+
+
+}
