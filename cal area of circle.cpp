1da55f6c2ec75fce1aecd7f75f61b3de454e93a2
Muhammad Hussain <118740883+MuhammadHussain003@users.noreply.github.com>
2022-11-29 19:37:09 +0500

    Create cal area of circle.cpp

---
+++ cal area of circle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ float radius;
+ const float PIE = 3.14;
+ cout<<"Enter radius of circle";
+ cin>>radius;
+ float area = PIE*radius*radius;
+ cout<<"Area of the circle is"<<area;
+
+
+}
