6333147646c320fb77352fd4c4b8c55d81415f56
Muhammad Hussain <118740883+MuhammadHussain003@users.noreply.github.com>
2022-12-06 22:01:50 +0500

    Create Array.cpp

---
+++ Array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{ 
+  int age[5];
+  for(int i=0;i<5;i++)
+  { 
+  	cout<<"Enter value number "<<i+1<<" is ";
+  	cin>>age[i];
+  
+  }
+   for(int i=0;i<5;i++)
+  { 
+  	cout<<"age number "<<i+1<<" is "<<age[i]<<endl;
+  	
+  
+  }
+ }
