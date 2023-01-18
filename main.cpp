5d775d1bbc70950ca0a20dff043f2301628cfe8f
Muhammad Hussain <118740883+MuhammadHussain003@users.noreply.github.com>
2023-01-18 21:13:28 +0500

    Add files via upload
    Constructor.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,8 @@
 #include<iostream>
-#include "Square.h"
-#include "Rectangle.h"
-#include "Circle.h"
+#include "MyClass.h"
 using namespace std;
 int main()
 {
-	int SArea;
-	int RArea;
-	
-	Square square;
-	SArea = square.Calculate_Area(3,6);
-	
-	Rectangle rectangle;
-	RArea = rectangle.Calculate_Area(2,5);
-	
-	cout<<"The Area of the Square is "<<SArea<<endl;
-	cout<<"The Area of the Rectangle is "<<RArea<<endl;
-    
-	Circle circle;
-	circle.ShowTheResult();
-}
+	MyClass obj;
+	obj.DisplayName();
+ } 
